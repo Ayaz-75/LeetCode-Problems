1766185e2a620af80dc82ed446cc8b8a3aa27dc8
Ayaz Ali <ayazalilakho2@gmail.com>
2025-01-12 17:27:49 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (26.78%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,15 +1,40 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Base cases
-        if not list1:
-            return list2
-        if not list2:
-            return list1
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode(-1)  # Create a dummy node
+        current = dummy       # Pointer to build the merged list
         
-        # Recursive step
-        if list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        # Append the remaining nodes
+        current.next = list1 if list1 else list2
+        
+        return dummy.next  # Return the head of the merged list
+
+
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         # Base cases
+#         if not list1:
+#             return list2
+#         if not list2:
+#             return list1
+        
+#         # Recursive step
+#         if list1.val < list2.val:
+#             list1.next = self.mergeTwoLists(list1.next, list2)
+#             return list1
+#         else:
+#             list2.next = self.mergeTwoLists(list1, list2.next)
+#             return list2
